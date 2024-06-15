8dca67d66d66d40aeaed4cc7bf90e304dd958f88
KC Flynn <38958974+flynnkc@users.noreply.github.com>
2024-06-15 16:53:33 -0400

    Logout, templates

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ import jwt
 from datetime import timedelta
 from os import getenv
 from cachelib import FileSystemCache
-from flask import Flask, request, session, redirect, url_for
+from flask import Flask, request, session, redirect, render_template, url_for
 from flask_session import Session
 from werkzeug import exceptions
 
@@ -22,11 +22,12 @@ Session(app)
 
 # Environment variable names
 client_id = getenv('CLIENT_ID')
-host = getenv('IDM_ENDPOINT')
+idm_host = getenv('IDM_ENDPOINT')
+app_host = getenv('APP_URI')
 client_secret = 'CLIENT_SECRET'
 
 # OIDC
-oidc_config = requests.get(f'{host}/.well-known/openid-configuration').json()
+oidc_config = requests.get(f'{idm_host}/.well-known/openid-configuration').json()
 signing_alg = oidc_config['id_token_signing_alg_values_supported']
 jwks_client = jwt.PyJWKClient(oidc_config['jwks_uri'])
 
@@ -34,16 +35,15 @@ jwks_client = jwt.PyJWKClient(oidc_config['jwks_uri'])
 
 @app.route('/', methods=['GET'])
 def home():
-    if not session.get('username'):
-        resp = '<h1>Welcome</h1>'
-        resp += f'<h2>{request.base_url}</h2>'
-        resp += f'<a href="{url_for("sign_in")}">Sign In</a>'
-        return resp
-    else:
-        resp = f'<h1>Welcome, {session["display_name"]}</h1>'
-        resp += f'<h2>{request.base_url}</h2>'
-        resp += f'<a href="{url_for("logout")}">Log out</a>'
-        return resp
+    if session.get('username'):
+        # TODO items logic
+        items = [{'foo': 'bar',
+             'boo': 'baz'}]
+        return render_template('index.html',
+                               name=session.get('display_name'),
+                               items=items)
+    
+    return render_template('index.html')
 
 @app.route('/signin', methods=['GET'])
 def sign_in():
@@ -56,9 +56,9 @@ def sign_in():
         session['nonce'] = nonce
         app.logger.debug(f'Session: {session.items()}')
 
-        url = f'{host}/oauth2/v1/authorize'
+        url = f'{idm_host}/oauth2/v1/authorize'
         url += f'?client_id={client_id}&response_type=code'
-        url += f'&redirect_uri=http://localhost:5000{url_for("callback")}'
+        url += f'&redirect_uri={app_host}{url_for("callback")}'
         url += f'&scope=openid&nonce={nonce}'
         url += f'&state={state}'
         app.logger.debug(f'Signin redirect URL: {url}')
@@ -74,7 +74,7 @@ def callback():
     
     code = request.args.get('code')
     if code:
-        r = requests.post(f'{host}/oauth2/v1/token',
+        r = requests.post(f'{idm_host}/oauth2/v1/token',
                       auth=(client_id, getenv(client_secret)),
                       data={'grant_type': 'authorization_code',
                             'code': code})
@@ -99,6 +99,7 @@ def callback():
 
         session['username'] = f'{data["domain"]}/{data["sub"]}'
         session['display_name'] = data['user_displayname']
+        session['id_token'] = tok
         
         return redirect(url_for('home'))
 
@@ -106,7 +107,12 @@ def callback():
 
 @app.route('/logout')
 def logout():
-    session.clear()
+    if session.get('username'):
+        url = f'{idm_host}/oauth2/v1/userlogout?id_token_hint={session.get("id_token")}'
+        url += f'&post_logout_redirect_uri={app_host}{url_for("home")}'
+        session.clear()
+        return redirect(url)
+    
     return redirect(url_for('home'))
 
 @app.errorhandler(exceptions.BadRequest)
