935dcb39007a4357af9457f232d3fd126f19b90d
KC Flynn <38958974+flynnkc@users.noreply.github.com>
2024-07-17 16:03:28 -0400

    Add more delete types

--- modules/delete/delete.py
+++ modules/delete/delete.py
@@ -3,6 +3,7 @@
 import logging
 from http import HTTPStatus
 
+from oci.analytics import AnalyticsClient
 from oci.bastion import BastionClient
 from oci.core import BlockstorageClient, ComputeClient
 from oci.oda import OdaClient
@@ -17,6 +18,7 @@ class Deleter:
         self.signer = signer
 
         # Clients start empty and are initialized as needed
+        self.analytics_client: AnalyticsClient | None = None
         self.bastion_client: BastionClient | None = None
         self.blockstorage_client: BlockstorageClient | None = None
         self.oda_client: OdaClient | None = None
@@ -38,10 +40,17 @@ class Deleter:
 
         # Use this dictionary to select the correct method for resource type
         self.control = {
+            'AnalyticsInstance': self.terminate_analytics_instance,
             'Instance': self.terminate_instance,
+            'DedicatedVmHost': self.terminate_dedicated_vm,
             'Image': self.terminate_image,
             'BootVolume': self.terminate_boot_volume,
+            'BootVolumeBackup': self.terminate_boot_volume_backup,
             'Volume': self.terminate_volume,
+            'VolumeBackup': self.terminate_volume_backup,
+            'VolumeBackupPolicy': self.terminate_volume_backup_policy,
+            'VolumeGroup': self.terminate_volume_group,
+            'VolumeGroupBackup': self.terminate_volume_group_backup,
             'AutonomousDatabase': self.terminate_autonomousdatabase,
             'DbSystem': self.terminate_dbsystem,
             'IntegrationInstance': self.terminate_integration_instance,
@@ -59,14 +68,28 @@ class Deleter:
             self.logger.debug(f'Calling {terminate_func.__name__}')
             return terminate_func(**resource)
         except KeyError:
+            self.logger.info(f'Resource type {resource["identifier"]} not supported')
             return HTTPStatus.NOT_IMPLEMENTED
+        
+    def terminate_analytics_instance(self, **kwargs) -> int:
+        if not self.analytics_client:
+            self.analytics_client = AnalyticsClient(self.config, signer=self.signer)
 
-    # Terminate instance resources
+        return self.analytics_client.delete_analytics_instance(
+            kwargs.get('identifier')).status
+
     def terminate_instance(self, **kwargs) -> int:
         if not self.instance_client:
             self.instance_client = ComputeClient(self.config, signer=self.signer)
 
         return self.instance_client.terminate_instance(
+            kwargs.get('identifier')).status
+    
+    def terminate_dedicated_vm(self, **kwargs) -> int:
+        if not self.instance_client:
+            self.instance_client = ComputeClient(self.config, signer=self.signer)
+
+        return self.instance_client.delete_dedicated_vm_host(
             kwargs.get('identifier')).status
     
     def terminate_image(self, **kwargs) -> int:
@@ -83,12 +106,52 @@ class Deleter:
         return self.blockstorage_client.delete_boot_volume(
             kwargs.get('identifier')).status
     
+    def terminate_boot_volume_backup(self, **kwargs) -> int:
+        if not self.blockstorage_client:
+            self.blockstorage_client = BlockstorageClient(self.config,
+                                                          signer=self.signer)
+            
+        return self.blockstorage_client.delete_boot_volume_backup(
+            kwargs.get('identifier')).status
+    
     def terminate_volume(self, **kwargs) -> int:
         if not self.blockstorage_client:
             self.blockstorage_client = BlockstorageClient(self.config,
                                                           signer=self.signer)
             
         return self.blockstorage_client.delete_volume(
+            kwargs.get('identifier')).status
+    
+    def terminate_volume_backup(self, **kwargs) -> int:
+        if not self.blockstorage_client:
+            self.blockstorage_client = BlockstorageClient(self.config,
+                                                          signer=self.signer)
+            
+        return self.blockstorage_client.delete_volume_backup(
+            kwargs.get('identifier')).status
+    
+    def terminate_volume_backup_policy(self, **kwargs) -> int:
+        if not self.blockstorage_client:
+            self.blockstorage_client = BlockstorageClient(self.config,
+                                                          signer=self.signer)
+            
+        return self.blockstorage_client.delete_volume_backup_policy(
+            kwargs.get('identifier')).status
+    
+    def terminate_volume_group(self, **kwargs) -> int:
+        if not self.blockstorage_client:
+            self.blockstorage_client = BlockstorageClient(self.config,
+                                                          signer=self.signer)
+            
+        return self.blockstorage_client.delete_volume_group(
+            kwargs.get('identifier')).status
+    
+    def terminate_volume_group_backup(self, **kwargs) -> int:
+        if not self.blockstorage_client:
+            self.blockstorage_client = BlockstorageClient(self.config,
+                                                          signer=self.signer)
+            
+        return self.blockstorage_client.delete_volume_group_backup(
             kwargs.get('identifier')).status
     
     def terminate_autonomousdatabase(self, **kwargs) -> int:
