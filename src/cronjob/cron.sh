cfba7f7548cc0ff13623ddaa7feefb0739ab6310
KC Flynn <38958974+flynnkc@users.noreply.github.com>
2025-01-06 09:08:46 -0500

    Remove unrelated code

--- src/cronjob/cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# For use on a VM/Bare Metal server
-
-crontab -l > cron
-echo "0 0 * * * python3 update.py" >> cron
-crontab cron
-rm cron
--- src/cronjob/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3.11
-
-from .tagupdater import TagUpdater
-from .signer import create_signer
