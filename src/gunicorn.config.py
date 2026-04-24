dc8931951b22ec318dab9a5eaa433c87d4f98020
KC Flynn <38958974+flynnkc@users.noreply.github.com>
2026-04-24 15:36:40 -0400

    Add retry to cost service

--- src/gunicorn.config.py
+++ src/gunicorn.config.py
@@ -7,12 +7,12 @@ bind = "unix:/run/gunicorn/gunicorn.sock"
 
 bind = "0.0.0.0:5000"
 
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() * 2
 
 loglevel = "info"
-#accesslog = "/var/log/gunicorn/access.log"
-#errorlog = "/var/log/gunicorn/error.log"
-#capture_output = True
 
 max_requests = 1000
 max_requests_jitter = 50
+
+timeout = 600 # Default OCI retry limit
+graceful_timeout = 60
